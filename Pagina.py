3d40ceebb2be405710c2ebe76fb53e63f3e8d81e
arthurbougleux <arthur_lbs@id.uff.br>
2023-11-10 13:18:22 -0300

    Incompleto

--- Pagina.py
+++ Pagina.py
@@ -1,4 +1,8 @@
 class Pagina():
+
     #deve haver um metodo que entregue uma copia dela mesma
-    def __init__(self, id) -> None:
-        self.id_processo = id
+    def __init__(self, tam) -> None:
+
+        # A página não precisa conhecer o id do próprio processo, certo? -Arthur
+        self.tam = tam
+        self.conteudo = []
