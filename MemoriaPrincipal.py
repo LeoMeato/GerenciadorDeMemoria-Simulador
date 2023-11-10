0d2fbac80c333f337f175045d519afa204573d6a
Fabio Gabriel <fbogabriel@outlook.com>
2023-11-10 11:58:26 -0300

    Atualizado antes do almoco sagrado :)

--- MemoriaPrincipal.py
+++ MemoriaPrincipal.py
@@ -3,31 +3,38 @@ from Swapper import *
 from TabelaDePaginas import *
 
 class MemoriaPrincipal:
-    quadros = []            #Quadros alocados em MP
+    quadros = []           #Quadros disponiveis e usados em MP
     tabelas_de_paginas = [] #Tabela de Paginas mantida
     
     
     def __init__(self, bits_size, bits_log):
         if bits_size < bits_log:
-            bits_log = bits_size
+            bits_log = bits_size#Evita o caso de o tamanho de enderecos do quadro for maior que o tamanho da MP, sendo assim, o quadro pode ser no maximo igual a MP
 
-        self.num_bit = bits_log
+        self.bits_log= bits_log
         self.bits_size = bits_size
-        self.tam_quadro = 2**bits_size
+        self.tam_quadro = 2**bits_log
 
-        self.quadros = self.mp_constroi_memoria(bits_log, bits_size)
+        self.quadros = self.constroi_memoria()
 
-            
-    def mp_constroi_memoria(self, bits_log, bits_size):#Inicializacao da paginacao da memoria
-        for i in range((2**bits_size)/(2**bits_log)):
-            self.quadros.append(Quadro(0))
+    #bits_size = n (Tamanho total da memoria)
+    # bits_log = m (Tamanho do quadro) 
+    def constroi_memoria(self, bits_log, bits_size):#Inicializacao da paginacao da memoria
+        for i in range((2**self.bits_size)/(2**self.bits_log)):
+            self.quadros.append(Quadro())#Setando o tempo de ultimo acesso como 0
 
 
-    def mp_retira_pagina(self, index_quadro):#recebo do swapper a pagina calculada a ser retirada
+    def retira_pagina(self, index_quadro):#recebo do swapper a pagina calculada a ser retirada
         self.quadros[index_quadro] = None
 
-    def mp_aloca_pagina(self, index_quadro, pagina):
-        self.quadros[index_quadro] = pagina
+    def aloca_pagina(self, index_quadro, pagina):
+        self.quadros[index_quadro] = pagina#E guardado a pagina trazida da memoria
 
-    def mp_consulta_pagina(self, index_quadro):#Obtem a pagina para swapper analisar, ou para devida manipulacao como swapp-in
-        return self.quadros[index_quadro]
+    def consulta_pagina(self, index_quadro):#Obtem a pagina para swapper analisar, ou para devida manipulacao como swapp-in
+        return self.quadros[index_quadro].consulta_quadro()#Retorna None se o quadro estiver vazio
+    
+    def consulta_tabela(self, index_tabela):
+        return self.tabelas_de_paginas[index_tabela] #Retorno a tabela de paginas para consultar qual quadro esta a pagina
+    
+    def tamanho_do_quadro(self):
+        return 2**self.bits_log#Numero de enderecos disponiveis no quadro
--- Quadro.py
+++ Quadro.py
@@ -1,6 +1,19 @@
 class Quadro:
+    pagina = None#Tem a referida pagina (e serve como bit de presenca)
+    def __init___(self):
+        self.last_update = 0
+    
+    def consulta_quadro(self):
+        return self.pagina#Retorna a pagina que esta alocada, ou None se nao ha nada
 
-    def __init___(self, last):
-        self.last_update = last
-        #print(clock)
-        print(1)
+    def aloca_pagina(self, pagina):
+        self.pagina = pagina#Usado para guardar pagina traga da memoria secundaria
+    
+    def desaloca_pagina(self):
+        self.pagina = None#Libera o quadro para entrada de outra pagina
+
+    def atualiza_clock(self):
+        self.last_update += 1#Vai incrementando o tempo do ultimo acesso da memoria
+    
+    def acessado(self):
+        self.last_update = 0#Seta o tempo como zero, pois foi usado a pagina recentemente
