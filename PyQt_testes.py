63f782ce442eeeaf2da92b7ce18fcf5a9662b8b2
Leonardo Meato <leomeato@gmail.com>
2023-11-10 21:36:30 -0300

    alguns testes com o PyQt

--- PyQt_testes.py
+++ PyQt_testes.py
@@ -1,10 +1,35 @@
 from PyQt5.QtWidgets import *
 
+global txt
+txt = 0
+
 def main():
+    def onClicked():
+        pass
     app = QApplication([])
     window = QWidget()
+    window.setGeometry(100, 100, 200, 300)
+    window.setWindowTitle("Teste")
+    window2 = QWidget()
+    
+
+    layout = QVBoxLayout()
+    label = QLabel("{}".format(txt))
+    textbox = QTextEdit()
+    button = QPushButton("Teste")
+    button.clicked.connect(onClicked)
+
+    layout.addWidget(label)
+    layout.addWidget(textbox)
+    layout.addWidget(button)
+
+    window.setLayout(layout)
+
     window.show()
+    #window2.show()
     app.exec_()
+
+    
 
 if __name__ == '__main__':
     main()
