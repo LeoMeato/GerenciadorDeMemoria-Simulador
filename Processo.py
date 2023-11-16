2c0d5b65662d9bce224c11fc417e65efb1b477c8
arthurbougleux <arthur_lbs@id.uff.br>
2023-11-15 21:22:04 -0300

    Criação de páginas com tamanho adequado

--- Processo.py
+++ Processo.py
@@ -2,17 +2,30 @@ from Pagina import Pagina, Agrupavel
 from TabelaDePaginas import *
 #from collections import deque
 
+#   Classe: Processo
+#
+#   Responsável por representar os processos recebidos de um escalonador imaginário
 class Processo(Agrupavel):
 
     label = "Sprites/Processo_Label.jpg"
     content = ""
 
-    # IMPORTANTE: Refazer a geração das páginas do processo baseado no tamanho real
+    #   Construtor
+    #
+    #   Fugindo do padrão, aqui os tamanhos são esperados como decimais para facilitar a coelta de informações do gerenciador
+    def __init__(self, tam, tam_quadro, id) -> None:
 
-    def __init__(self, tam, id) -> None:
         self.id = id
         self.content = "Processo {}".format(id)
-        self.paginas = [Pagina(id)]
+
+        self.paginas = []
+
+        #IMPORTANTE: Lembra de revisar essa palhaçada Arthur
+        for i in range(tam//tam_quadro):
+
+            self.paginas.append(Pagina(tam_quadro))
+        self.paginas.append(Pagina(tam % tam_quadro))
+
         self.pcb = Pcb(id, tam)
 
     
