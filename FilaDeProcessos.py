5fd1e0b72563e61071deb8e48abcda4044055630
arthurbougleux <arthur_lbs@id.uff.br>
2023-11-15 21:19:11 -0300

    Declaração de métodos necessários

--- FilaDeProcessos.py
+++ FilaDeProcessos.py
@@ -4,21 +4,24 @@
 
 #   Classe: Fila de Processos
 #
-#   Representação estruturada das filas de pronto, 
-#
+#   Representação estruturada das filas de novo, pronto, bloqueado, etc
 class FilaDeProcessos:
 
     def __init__(self):
 
+        self.novo = Fila()
         self.pronto = Fila()
         self.bloqueado = Fila()
         self.sus_pronto = Fila()
         self.sus_bloqueado = Fila()
-        
+
+    def transita(pid, fila1, fila2):
+        pass
+
 
 class Fila:
 
-    def __init__(self, maxLength=None) -> None:
+    def __init__(self) -> None:
         self.fila = []
 
     def adicionar(self, processo):
--- Gerenciador.py
+++ Gerenciador.py
@@ -2,6 +2,8 @@
 #   da memória do sistema e dos componentes associados
 
 from Swapper import *
+from Processo import *
+from FilaDeProcessos import *
 from MemoriaPrincipal import *
 from MemoriaSecundaria import *
 
@@ -14,7 +16,9 @@ class Gerenciador:
     #
     #   Inicializa o gerenciador explicitando os tamanhos dos vários componentes da memória
     #   Tamanhos são recebidos no formato log2(tamanho desejado) por padrão
-    def __init__(self, bits_mp, bits_ms, bits_log, arq_entrada):
+    #   n_residente_inicial se refere ao tamanho do conjunto residente alocado na criação de um processo
+    #   que passa pela transição (novo, pronto)
+    def __init__(self, bits_mp, bits_ms, bits_log, n_residente, arq_entrada):
         
         #   A variável clock é usada como indicação de tempo para a implementação de políticas
         #   do gerenciador e coleta de dados de execução
@@ -24,12 +28,93 @@ class Gerenciador:
         self.bits_ms = bits_ms
         self.bits_log = bits_log
 
+        self.n_resident = n_residente
+
         self.arq_entrada = arq_entrada
 
         self.MP = MemoriaPrincipal(bits_mp, bits_log)
         self.MS = MemoriaSecundaria(bits_ms)
         self.TP = []
 
+        #self.tabela_de_processos = [] Isso é válido?
+
+        self.fila_de_processos = FilaDeProcessos()
+
+
+    #   Método responsável por alocar as estruturas necessárias para administrar um novo processo
+    def cria_processo(self, pid, size):
+
+        p = Processo(size, self.MP.tam_quadro, pid)
+        self.fila_de_processos.novo.adicionar(p)
+
+    #   Método responsável por carregar a imagem de um processo novo para a MP
+    #   O número de quadros carregados é decidido por uma configuração
+    # def carrega_imagem(self, pid):
+
+    #     tabela = None
+
+    #     for i in range(len(self.TP)):
+    #         if self.TP[i].id == pid:
+    #             tabela = self.TP[i]
+
+    #     for i in range(self.n_resident):
+
+    #         self.MP.add_LRU(tabela.registros[i])
+    #         tabela.registros[i].p = True
     
-    def cria_processo(size):
+
+    #   Método utilizado para marcar a passagem do tempo dentro do sistema
+    #   Necessário para a implementação da política LRU de substituição de páginas
+    #   e para a chamda do swapper
+    def atualizaClock(self):
+
+        self.clock += 1
+        self.MP.atualizaClock()
+    
+
+    #   Método utilizado para suspender um processo, que será escolhido pelo swapper
+    #   Deve ser chamado quando a taxa de falta de páginas atingir um número inaceitável
+    def callSwapOut(self):
+        
+        pass
+
+    #   Método utilizado para cancelar a suspensão de um processo escolhido pelo swapper
+    def unsuspend(self, pid):
+        
+        pass
+
+    #   Método utilizado para indicar um erro fatal para a execução do programa
+    #   Chamado em situações absurdas como a submissão de um processo com tamanho maior
+    #   que o endereço lógico, falta de espaço para swap, acesso à um endereço inválido, etc
+    def erro_fatal(msg):
+
+        pass
+
+    #   Os métodos a seguir representam requisições básicas feitas por processos
+    #   Comumente resultam em faltas de páginas, visto que acessam a MP
+    #   Recebem as identificações dos processos e endereços lógicos (quando aplicável)
+    def adquireCPU(self, pid, end):
+
+        pass
+    
+    def IOinstruction(self, pid):
+
+        #Ver amanhã se eu posso roubar com uma lista de processos. Até lá assumir que p existe
+        p = Processo("LEMBRAR DE REMOVER ISSO")
+
+        p.pcb.setBloqueado()
+        self.fila_de_processos.transita(pid, "pronto", "bloqueado")
+
+        pass
+
+    def leituraMP(self, pid, end):
+
+        pass
+
+    def escritaMP(self, pid, end):
+
+        pass
+
+    #   Método utilizado para desalocar as estruturas associadas a um processo após o seu fim
+    def terminateProcess(self, pid):
         pass
