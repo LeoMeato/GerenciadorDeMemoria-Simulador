09582da6b1fabab9e5250a0c7e62855673729478
Leonardo Meato <leomeato@gmail.com>
2023-11-19 18:08:11 -0300

    limpeza

--- main.py
+++ main.py
@@ -12,10 +12,27 @@ janela.set_title("Gerenciador de Memória")
 Mouse = janela.get_mouse()
 teclado = janela.get_keyboard()
 
+global tam_mp
+global tfp
+global mdpf
+
+global ms
+global mp
+global cpu
+
+tam_mp = 16
+tfp = 0
+mdpf = 0
+
+ms = Coluna(1475, 30, "MS")
+mp = Coluna(1650, 30, "MP")
+cpu = Coluna(1250, 30, "CPU");cpu.add(Container("Sprites/Cpu_Label.jpg"))
+
+'''
 #Leitura de Arquivo
 arquivo = open("nome_arq", 'r')
 instrucoes = arquivo.readlines()
-arquivo.close
+arquivo.close'''
 """
 processos = []
 id_processos = 0
@@ -62,48 +79,20 @@ coluna.pop(5)
 coluna.absoluteMove(20, 20)
 coluna.relativeMove(10, 10)'''
 
-MS = Coluna(1475, 30, "MS")
 
-p1 = Processo(16, 4, 0)
-p2 = Processo(4, 4, 1)
-p3 = Processo(4, 4, 2)
-p4 = Processo(4, 4, 3)
 
 processos = []
-for c in range(5, 40):
-    processos.append(Processo(20, 2, c))
+for i in range(40):
+    processos.append(Processo(20, 2, i))
 
-g1 = Grupo(p1, janela)
-g2 = Grupo(p2, janela)
-g3 = Grupo(p3, janela)
-g4 = Grupo(p4, janela)
+for i in range(tam_mp):
+    mp.add(Container("Sprites/Quadro_Label.jpg"))
 
-MS.add(g1)
-MS.add(g2)
-MS.add(g3)
-MS.add(g4)
-
-MS.remove(1)
-MS.overwrite(1, g2)
-
-MP = Coluna(1650, 30, "MP")
-for i in range(16):
-    MP.add(Container("Sprites/Quadro_Label.jpg"))
-for i in range(4):
-    MP.array[i].setContent(Grupo(p1.paginas[i], janela))
-
-cpu = Coluna(1250, 30, "CPU")
-cpu.add(Container("Sprites/Cpu_Label.jpg"))
-cpu.array[0].setContent(Grupo(p2, janela))
-
-filaProntos = Coluna(20, 400)
-for i in range(3):
-    filaProntos.add(Grupo(processos[i], janela))
-
+nomesDasFilas = ("Novo", "Pronto", "Bloq. Page Fault", "Bloq. por E/S", "Pronto-Suspenso", "Bloq.-Suspenso", "???")
 filas = []
 for i in range (7):
-    filas.append(Coluna(10 + i * 165, 500))
-    for j in range(2):
+    filas.append(Coluna(10 + i * 165, 500, nomesDasFilas[i]))
+    '''for j in range(2):
         filas[i].add(Grupo(processos[i*2 + j], janela))
 
 filas[3].add(Grupo(processos[14], janela))
@@ -112,7 +101,7 @@ filas[3].add(Grupo(processos[16], janela))
 filas[3].add(Grupo(processos[18], janela))
 filas[3].add(Grupo(processos[19], janela))
 filas[5].add(Grupo(processos[20], janela))
-filas[5].add(Grupo(processos[21], janela))
+filas[5].add(Grupo(processos[21], janela))'''
 
 
 '''
@@ -133,19 +122,18 @@ for c in range(3):
 print(t.id, t.registros[6].numQuadro)
 print(f.pronto.fila)'''
 
-tfp = 3
-mdpf = 4
 
 while True:
     janela.set_background_color([255, 255, 255])
-    MS.draw()
-    MP.draw()
-    MS.draw_text(janela)
-    MP.draw_text(janela)
+    ms.draw()
+    mp.draw()
+    ms.draw_text(janela)
+    mp.draw_text(janela)
     cpu.draw()
     cpu.draw_text(janela)
     for i in filas:
         i.draw()
+        i.draw_text(janela)
     write(janela, f"Taxa de falta de páginas: {tfp};;Memória desperdiçada por fragmentação: {mdpf}", 40, 40, 20)
     janela.update()
     #erro("oops")
