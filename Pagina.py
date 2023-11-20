f1e83d0c6bcc5cc203a4e0d2d13bbfd2dad02022
arthurbougleux <arthur_lbs@id.uff.br>
2023-11-20 19:51:09 -0300

    Comentários para Cristina

--- Pagina.py
+++ Pagina.py
@@ -13,8 +13,12 @@ class Pagina(Agrupamento):
     #deve haver um metodo que entregue uma copia dela mesma
     def __init__(self, tam, id, num) -> None:
 
+        #Informações que poderiam ser consultadas pela tabela de páginas são
+        #redundadas para permitir o funcionamento da interface
         self.id = id
         self.num = num
         self.content = f"Página {num} P{id}"
+
+
         self.tam = tam
         self.conteudo = []
