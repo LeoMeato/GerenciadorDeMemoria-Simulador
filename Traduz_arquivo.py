e185ee903fde3a60336e4f360b04062bf47d9614
arthurbougleux <arthur_lbs@id.uff.br>
2023-11-22 17:17:34 -0300

    Commit

--- Traduz_arquivo.py
+++ Traduz_arquivo.py
@@ -9,15 +9,21 @@ def traduz(path):
     for i in range(len(texto)):
         parcial = []
         temp = texto[i].split()
-        parcial.append(temp[0])
-        parcial.append(temp[1])
+        parcial.append(str(temp[0]))
+        parcial.append(str(temp[1]))
         if len(temp) == 4:
-            parcial.append(decimal_binario(temp[2]))
+            parcial.append(str(decimal_binario(temp[2])))
         texto_sub.append(parcial)
 
     arq = open("bin.txt", "w")
     
+    
     for i in range(len(texto_sub)):
-        arq.write(texto_sub[i][0] +" "+ texto_sub[i][1] + " " + str(texto_sub[i][2]+"\n"))
+        text = texto_sub[i][0] +" "+ texto_sub[i][1]
+        if len(texto_sub[i]) == 3:
+            text = text + " " + texto_sub[i][2]+"\n"
+        else:
+            text = text +"\n"
+        arq.write(text)
 
     arq.close()
